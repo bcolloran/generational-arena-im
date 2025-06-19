f3783ea609d9c275aad968babe9ac23602d01c34
brendan c <brendan@colloran.net>
2025-06-18 22:05:46 -0700

    update tests, make rayon default

--- tests/parallel_tests.rs
+++ tests/parallel_tests.rs
@@ -11,10 +11,9 @@ fn par_iter_matches_sequential() {
     for i in 0..100 {
         arena.insert(i);
     }
-    let mut seq: Vec<_> = arena.iter().map(|(_, v)| *v).collect();
-    let mut par: Vec<_> = (&arena).par_iter().map(|(_, v)| *v).collect();
-    seq.sort();
-    par.sort();
+    let seq: Vec<_> = arena.iter().map(|(_, v)| *v).collect();
+    let par: Vec<_> = (&arena).par_iter().map(|(_, v)| *v).collect();
+
     assert_eq!(seq, par);
 }
 
@@ -25,7 +24,6 @@ fn par_iter_mut_updates() {
         arena.insert(i);
     }
     (&mut arena).par_iter_mut().for_each(|(_, v)| *v *= 2);
-    let mut values: Vec<_> = arena.iter().map(|(_, v)| *v).collect();
-    values.sort();
+    let values: Vec<_> = arena.iter().map(|(_, v)| *v).collect();
     assert_eq!(values, (0..100).map(|x| x * 2).collect::<Vec<_>>());
 }
