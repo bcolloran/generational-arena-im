1dd341902d0f6770b33a505bcd34f6b8f1b28e22
brendan c <brendan@colloran.net>
2025-06-22 15:18:58 -0700

    clean up tests

--- tests/parallel_tests.rs
+++ tests/parallel_tests.rs
@@ -1,7 +1,9 @@
 extern crate generational_arena_im;
 extern crate rayon;
 use generational_arena_im::StandardArena as Arena;
-use rayon::iter::{IntoParallelIterator, ParallelIterator};
+use rayon::iter::{
+    IntoParallelIterator, IntoParallelRefIterator, IntoParallelRefMutIterator, ParallelIterator,
+};
 
 #[test]
 fn par_iter_matches_sequential() {
@@ -9,14 +11,38 @@ fn par_iter_matches_sequential() {
     for i in 0..100 {
         arena.insert(i);
     }
+
     let seq: Vec<_> = arena.iter().map(|(_, v)| *v).collect();
-    let par: Vec<_> = (&arena).into_par_iter().map(|(_, v)| *v).collect();
+    let par: Vec<_> = arena.par_iter().map(|(_, v)| *v).collect();
+
+    assert_eq!(seq, par);
+}
+
+#[test]
+fn into_par_iter_matches_sequential() {
+    let mut arena = Arena::new();
+    for i in 0..100 {
+        arena.insert(i);
+    }
+    let seq: Vec<_> = arena.iter().map(|(_, v)| *v).collect();
+    let par: Vec<_> = arena.into_par_iter().map(|(_, v)| *v).collect();
 
     assert_eq!(seq, par);
 }
 
 #[test]
 fn par_iter_mut_updates() {
+    let mut arena = Arena::new();
+    for i in 0..100 {
+        arena.insert(i);
+    }
+    arena.par_iter_mut().for_each(|(_, v)| *v *= 2);
+    let values: Vec<_> = arena.iter().map(|(_, v)| *v).collect();
+    assert_eq!(values, (0..100).map(|x| x * 2).collect::<Vec<_>>());
+}
+
+#[test]
+fn into_par_iter_mut_updates() {
     let mut arena = Arena::new();
     for i in 0..100 {
         arena.insert(i);
