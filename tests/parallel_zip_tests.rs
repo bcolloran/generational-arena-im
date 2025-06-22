d3db51c18621b254197ece1bc0f6b1b6577cc8e6
brendan c <brendan@colloran.net>
2025-06-22 16:32:52 -0700

    add failing parallel_zip_test.rs to repo

---
+++ tests/parallel_zip_tests.rs
@@ -0,0 +1,28 @@
+extern crate generational_arena_im;
+extern crate rayon;
+use generational_arena_im::StandardArena as Arena;
+use rayon::iter::{
+    IndexedParallelIterator, IntoParallelIterator, IntoParallelRefIterator,
+    IntoParallelRefMutIterator, ParallelIterator, Zip,
+};
+
+#[test]
+fn par_zip_test() {
+    let mut arena_a = Arena::new();
+    let mut arena_b = Arena::new();
+
+    for i in 0..100 {
+        arena_a.insert(i);
+        arena_b.insert(i * 2);
+    }
+
+    let seq: Vec<_> = (0..100u32).into_iter().map(|v| v * 3).collect();
+
+    let par: Vec<_> = arena_a
+        .par_iter()
+        .zip(arena_b.par_iter())
+        .map(|((_, a), (_, b))| a + b)
+        .collect();
+
+    assert_eq!(seq, par);
+}
